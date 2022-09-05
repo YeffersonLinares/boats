0855f580ecd71b21e14c23bf1892acc55356563a
Yefferson Linares <ylinares@unbosque.edu.co>
2022-09-05 15:41:37 -0500

    validacion

--- validar_horas.php
+++ validar_horas.php
@@ -12,8 +12,8 @@ function validar_horas($wpdb, $fecha, $id_vote)
         $sql = "SELECT * FROM reserva WHERE fecha='$fecha' AND id_vote='$id_vote'";
 
         $data = mysqli_query($wpdb, $sql);
-        $reserva = mysqli_fetch_array($data);
-        return json_encode($reserva);
+        $reserva = map(mysqli_fetch_all($data));
+        return $reserva;
     } catch (\Throwable $th) {
         $data = [
             'line' => $th->getLine(),
@@ -31,8 +31,8 @@ function map($data)
     $array = [];
     $index = 0;
     foreach ($data as $key => $value) {
-        $array[$index]['hora_fin'] = $value[5];
-        $array[$index]['hora_inicio'] = $value[6];
+        $array[$index]['hora_fin'] = $value[6];
+        $array[$index]['hora_inicio'] = $value[5];
         $array[$index]['cant_adultos'] = $value[7];
         $array[$index]['cant_bebes'] = $value[8];
         $array[$index]['cant_tres_doce'] = $value[9];
@@ -41,4 +41,28 @@ function map($data)
     return $array;
 }
 
-echo validar_horas($wpdb, $fecha, $id_vote);
+function response($reservas)
+{
+    $response = true;
+    $ad = null;
+    foreach ($reservas as $value) :
+        $ad = $value['hora_inicio'];
+        $ad2 = $value['hora_fin'];
+        // if ($value['hora_inicio'] <='10:30' && $value['hora_fin'] >='10:30') {
+        if ($value['hora_inicio'] <= '07:30:00' && $value['hora_fin'] >='07:30:00') {
+            $response = false;
+        }
+        if ($value['hora_inicio'] <= '08:30:00' && $value['hora_fin'] >= '08:30:00') {
+            $response = false;
+        }
+    endforeach;
+    $data = [
+        'desocupado' => $response,
+        '$ad' => $ad,
+        'ad2' => $ad2,
+    ];
+    return json_encode($data);
+}
+
+// echo validar_horas($wpdb, $fecha, $id_vote);
+echo response(validar_horas($wpdb, $fecha, $id_vote));
