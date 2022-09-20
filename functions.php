ad08f567319117bdc9de5f56833f43ffffbc63a6
Yefferson Linares <ylinares@unbosque.edu.co>
2022-09-20 10:50:06 -0500

    visual barcos

--- functions.php
+++ functions.php
@@ -3,6 +3,7 @@
 ?>
 <script src="//unpkg.com/alpinejs" defer></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/axios/0.19.2/axios.min.js"></script>
+<link rel="stylesheet" href="./wp-content/themes/twentytwentytwo/style.css">
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css" crossorigin="anonymous">
 <link href='https://fonts.googleapis.com/css?family=Titillium+Web:400,200,300,600,700' rel='stylesheet' type='text/css'>
@@ -3134,7 +3135,6 @@
                 color: white !important;
                 padding: .5rem 1rem .5rem 1rem;
                 box-shadow: none;
-                size-img-grid
             }
 
             @media(max-width: 992px) {
