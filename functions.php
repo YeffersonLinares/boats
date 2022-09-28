e1293c684f9fa0d00bc81216fd748c31d923363f
Yefferson Linares <ylinares@unbosque.edu.co>
2022-09-28 17:37:47 -0500

    datepicker

--- functions.php
+++ functions.php
@@ -11,6 +11,7 @@
 <script src="https://cdn.jsdelivr.net/npm/popper.js@1.14.7/dist/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.29.4/moment.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/datepicker/1.0.10/datepicker.min.js"></script>
 <script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>
 
 
@@ -20,7 +21,7 @@
 
 <div x-data="app_alpine()">
 
-    <!-- <div class="timeline d-flex fixed-top bg-white pt-5">
+    <div class="timeline d-flex fixed-top bg-white pt-5">
         <div class="events">
             <ol>
                 <ul>
@@ -65,7 +66,7 @@
                 </ul>
             </ol>
         </div>
-    </div> -->
+    </div>
 
     <?php
 
@@ -5007,7 +5008,7 @@
             //         headers
             //     })
             // },
-            pantalla: 'ReservaUsuarioExtra',
+            pantalla: 'ReservaUsuarioFecha',
             base_url: 'https://192.168.5.115:443',
             // base_url: 'https://192.168.5.115:443',
             // headers: { Authorization: `Bearer ${token}` }
