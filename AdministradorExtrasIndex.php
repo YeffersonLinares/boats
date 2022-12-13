78134335049e0b64676fc14c7d86dc5458e8a8b3
Yefferson Linares <ylinares@unbosque.edu.co>
2022-12-13 13:07:01 -0500

    Extra completado

--- AdministradorExtrasIndex.php
+++ AdministradorExtrasIndex.php
@@ -7,8 +7,8 @@
                     <hr>
                 </div>
             </div>
-            <div class="d-flex justify-content-end">
-                <button class="btn-blue-tareas">Agregar Nuevo Extra <i class="fa-solid fa-circle-arrow-right"></i></button>
+            <div class="d-flex justify-content-end mt-3">
+                <button class="btn-blue-tareas" @click="create()">Agregar Nuevo Extra <i class="fa-solid fa-circle-arrow-right"></i></button>
             </div>
             <div class="row mb-5">
                 <div class="col-12 d-flex align-items-center">
@@ -18,9 +18,9 @@
             </div>
             <div class="row px-4">
                 <template x-for="(i,index) in extras">
-                    <div class="col-md-6 col-lg-3 mb-3" role="button">
+                    <div class="col-md-6 col-lg-3 mb-3">
                         <div class="card card_cliente min-height37rem">
-                            <img :src="i.imagen1" alt="...">
+                            <img class="image-card-nyg" :src="view_image(i.imagenes_active?.src)" alt="..." role="button" @click="edit(i.id)">
                             <div class="card-body color-dark-extras">
                                 <h5 class="card_cliente-title" x-text="i.nombre"></h5>
                                 <hr>
@@ -33,7 +33,7 @@
                                     <template x-for="(item,position) in i.detalles">
                                         <div class="d-flex justify-content-between mb-1">
                                             <span x-text="'tamaño ' + item.descripcion">Tamaño S</span>
-                                            <span x-text="'+' + item.valor">+30€</span>
+                                            <span x-text="'+' + item.valor + '€'">+30€</span>
                                         </div>
                                     </template>
                                 </div>
@@ -61,10 +61,11 @@
 <script>
     function AdministradorExtrasIndex() {
         return {
-            pantalla: 'create',
+            pantalla: 'index',
+            edit_form: null,
+            // pantalla: 'create',
             extras: [],
             init() {
-                // $('#nav-clientes-tab').click()
                 this.getExtras()
             },
             getExtras() {
@@ -73,6 +74,23 @@
                     this.extras = res.data.extras
                 })
             },
+            create() {
+                this.pantalla = 'create'
+            },
+            view_image(src) {
+                if (src) {
+                    if (src.substr(0, 4) == 'http' || src.substr(0, 4) == 'data') return src
+                    else return this.base_url + src
+                } else return src
+            },
+            edit(id) {
+                let url = this.base_url + '/administrador/edit_extra/' + id
+                axios.get(url).then(res => {
+                    this.edit_form = res.data
+                    this.pantalla = 'create'
+                    // this.pantalla = 'creat    e'
+                })
+            }
         }
     }
 </script>
